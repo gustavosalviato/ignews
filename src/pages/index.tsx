00dd22440959abcde65ece2de55a37d5f062685e
gustavosalviato <gustavosalviato8@gmail.com>
2023-02-09 10:00:58 +0100

    Página HOme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
+import Image from 'next/image'
 export default function Home() {
   return (
     <>
@@ -6,9 +8,20 @@ export default function Home() {
         <title>Inicio ig.news</title>
       </Head>
 
-      <div>
-        <p>Hello world</p>
-      </div>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 hey, welcome</span> <br />
+
+          <h1>News about the <span>React</span> World</h1>
+
+
+          <p>Get acess to all the publications <br />
+            <span>for $9,90 month</span>
+          </p>
+        </section>
+
+        <Image src="/images/avatar.svg" alt="Girl Coding" width={334} height={520} />
+      </main>
     </>
   )
 }
