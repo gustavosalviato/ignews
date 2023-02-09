1b5f0c0b60cc6f9a13296523e273c999307c5cf7
gustavosalviato <gustavosalviato8@gmail.com>
2023-02-09 15:08:02 +0100

    SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from './home.module.scss'
 import Image from 'next/image'
+import { SubscribeButton } from '@/components/SubscribeButton'
 export default function Home() {
   return (
     <>
@@ -18,6 +19,8 @@ export default function Home() {
           <p>Get acess to all the publications <br />
             <span>for $9,90 month</span>
           </p>
+
+          <SubscribeButton/>
         </section>
 
         <Image src="/images/avatar.svg" alt="Girl Coding" width={334} height={520} />
